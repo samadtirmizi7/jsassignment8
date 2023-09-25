b0c069e960565bffc212aa0629eed9bf30aa5ee1
Syed Abdul Samad Tirmizi <144122288+samadtirmizi7@users.noreply.github.com>
2023-09-25 17:38:19 +0500

    Add files via upload

---
+++ js assignment 8/app.js
@@ -0,0 +1,49 @@
+// Chap 21 
+// task 1
+// var userInput = prompt();
+// var allLower = userInput.toLowerCase();
+// console.log(allLower);
+// task 2
+// var x = "Hello World!";
+// x = x.toLowerCase();
+// console.log(x);    
+// task 3
+// var y = "Hello World!";
+// y = y.toUpperCase(); 
+// console.log(y);
+// task 4 
+// var originalString = "Hello World";
+// var lowerCaseString = originalString.toLowerCase();
+// console.log(originalString);
+// task 5
+// var myArray = ["Hello", "World"];
+// var lowerCaseElement = myArray[0].toLowerCase();
+// console.log(myArray);
+// task 6
+// var myString = "Hello World";
+// var upperCaseString = myString.toUpperCase();
+// alert(upperCaseString);
+// task 7
+// var cityName = "kaRacHi";
+// var capitalizedName = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+// console.log(capitalizedName);
+
+// Chap 22-25
+// task 1
+
+var sameWords = "captain";
+var slicedWord = sameWords.slice(1, 3);
+console.log(slicedWord);
+
+// task 2
+
+var sameWords = "captain";
+var numberOfCharacters = sameWords.length;
+console.log(numberOfCharacters);
+
+// task 4
+
+var myString = "This is a sample string";
+var numberOfCharacters = myString.length;
+var secondVariable = numberOfCharacters;
+console.log(secondVariable);
